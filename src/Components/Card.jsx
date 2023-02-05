61074d77a49652d25f79f022d95fba5a191ec634
Rhutik Sahu <shubhamsahu938@gmail.com>
2023-02-05 13:17:54 +0100

    card_changes_made_ Final_submission

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -3,11 +3,19 @@ import React from 'react'
 
 const Card = ({ item }) => {
   return (
-    <div className='flex flex-col' key={item.id}>
-      <img src={item.thumbnailUrl} className="w-16 h-16" />
-      <h1>
-        {item.id}
-      </h1>
+    <div className='max-w-xs rounded overflow-hidden shadow-lg' key={item.id}>
+      <img src={item.thumbnailUrl} className="w-full" />
+      <div className='px-6 py-4'>
+        <h1 className='font-bold text-xl mb-2'>
+           {item.title}
+        </h1>
+        <p className='text-gray-700 text-base'>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Placeat accusamus dolores aut iusto, sapiente quod sed at ullam dolor maiores quos esse, reprehenderit temporibus veritatis magni omnis aspernatur ut expedita?
+        </p>
+      </div>
+      <div className='px-6 pt-4 pb-2'>
+      <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">3{item.id}</span>
+      </div>
     </div>
   )
 }
--- src/Components/Dashboard.jsx
+++ src/Components/Dashboard.jsx
@@ -62,7 +62,7 @@ const Dashboard = () => {
                             </ul>
                         </nav>
 
-                        <div className="grid grid-cols-3 gap-4">
+                        <div className="grid gap-x-20 gap-y-10 grid-cols-1 md:grid-cols-2 place-content-center mx-auto lg:grid-cols-3 gap-4">
                             {data && data.map((item, id) => {
                                 return (<Card key={id} item={item} />)
                             })
